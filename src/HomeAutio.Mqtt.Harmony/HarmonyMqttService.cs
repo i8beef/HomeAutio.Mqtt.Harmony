e36698fafac883b7e3deac58201341e8deb24404
Michael Hallock <mike@hallock.net>
2017-06-11 19:07:39 -0400

    Adds propper error handling and logging around everything.

--- src/HomeAutio.Mqtt.Harmony/HarmonyMqttService.cs
+++ src/HomeAutio.Mqtt.Harmony/HarmonyMqttService.cs
@@ -56,9 +56,17 @@ namespace HomeAutio.Mqtt.Harmony
         /// </summary>
         public override void StartService()
         {
-            // Connect to Harmony
-            _client.Connect();
-            GetConfig();
+            try
+            {
+                // Connect to Harmony
+                _client.Connect();
+                GetConfig();
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw;
+            }
         }
 
         /// <summary>
@@ -66,7 +74,15 @@ namespace HomeAutio.Mqtt.Harmony
         /// </summary>
         public override void StopService()
         {
-            _client.Dispose();
+            try
+            {
+                _client.Dispose();
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw;
+            }
         }
 
         #endregion
@@ -80,19 +96,28 @@ namespace HomeAutio.Mqtt.Harmony
         /// <param name="e"></param>
         protected override void Mqtt_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
-            var message = Encoding.UTF8.GetString(e.Message);
-            _log.Debug("MQTT message received for topic " + e.Topic + ": " + message);
+            try
+            {
+                var message = Encoding.UTF8.GetString(e.Message);
+                _log.Debug("MQTT message received for topic " + e.Topic + ": " + message);
 
-            if (e.Topic == _topicRoot + "/activity/set")
-            {
-                var activity = _harmonyConfig.Activity.FirstOrDefault(x => x.Label == message);
-                if (activity != null)
-                    _client.StartActivityAsync(int.Parse(activity.Id)).GetAwaiter().GetResult();
+                if (e.Topic == _topicRoot + "/activity/set")
+                {
+                    var activity = _harmonyConfig?.Activity?.FirstOrDefault(x => x.Label == message);
+                    if (activity != null)
+                        _client.StartActivityAsync(int.Parse(activity.Id)).GetAwaiter().GetResult();
+                }
+                else if (_topicActionMap.ContainsKey(e.Topic))
+                {
+                    var command = _topicActionMap[e.Topic];
+                    if (command != null)
+                        _client.SendCommandAsync(command);
+                }
             }
-            else if (_topicActionMap.ContainsKey(e.Topic))
+            catch (Exception ex)
             {
-                var command = _topicActionMap[e.Topic];
-                _client.SendCommandAsync(command);
+                _log.Error(ex);
+                throw;
             }
         }
 
@@ -107,10 +132,18 @@ namespace HomeAutio.Mqtt.Harmony
         /// <param name="e"></param>
         private void Harmony_CurrentActivityUpdated(object sender, int activityId)
         {
-            var currentActivity = _harmonyConfig.Activity.FirstOrDefault(x => x.Id == activityId.ToString())?.Label;
-            _log.Debug("Harmony current activity updated: " + currentActivity);
+            try
+            {
+                var currentActivity = _harmonyConfig.Activity.FirstOrDefault(x => x.Id == activityId.ToString())?.Label;
+                _log.Debug("Harmony current activity updated: " + currentActivity);
 
-            _mqttClient.Publish(_topicRoot + "/activity", Encoding.UTF8.GetBytes(currentActivity), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+                _mqttClient.Publish(_topicRoot + "/activity", Encoding.UTF8.GetBytes(currentActivity), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+                throw;
+            }
         }
 
         /// <summary>
@@ -118,66 +151,74 @@ namespace HomeAutio.Mqtt.Harmony
         /// </summary>
         private void GetConfig()
         {
-            _harmonyConfig = _client.GetConfigAsync().GetAwaiter().GetResult();
-
-            // Wipe topic to Harmony action map for reload
-            if (_topicActionMap.Count > 0)
-                _topicActionMap.Clear();
-
-            var hub = new Hub();
-
-            // Map all devices at {_topicRoot}/devices/{deviceLabel}/{controlGroup}/{controlName}/set
-            // Listen at topic {_topicRoot}/devices/+/+/+/set
-            foreach (var harmonyDevice in _harmonyConfig.Device)
+            try
             {
-                var device = new Device { Name = harmonyDevice.Label };
-                foreach (var controlGroup in harmonyDevice.ControlGroup)
-                {
-                    foreach (var control in controlGroup.Function)
-                    {
-                        var commandTopic = $"{_topicRoot}/devices/{harmonyDevice.Label.Sluggify()}/{controlGroup.Name.Sluggify()}/{control.Name.Sluggify()}/set";
-                        device.Controls.Add(new ButtonControl { Name = controlGroup.Name + " " + control.Name, CommandTopic = commandTopic });
+                _harmonyConfig = _client.GetConfigAsync().GetAwaiter().GetResult();
 
-                        // Add mapping for subscribed topic to Harmony control action, ignoring duplicates
-                        if (!_topicActionMap.ContainsKey(commandTopic))
-                            _topicActionMap.Add(commandTopic, control.Action);
+                // Wipe topic to Harmony action map for reload
+                if (_topicActionMap.Count > 0)
+                    _topicActionMap.Clear();
+
+                var hub = new Hub();
+
+                // Map all devices at {_topicRoot}/devices/{deviceLabel}/{controlGroup}/{controlName}/set
+                // Listen at topic {_topicRoot}/devices/+/+/+/set
+                foreach (var harmonyDevice in _harmonyConfig.Device)
+                {
+                    var device = new Device { Name = harmonyDevice.Label };
+                    foreach (var controlGroup in harmonyDevice.ControlGroup)
+                    {
+                        foreach (var control in controlGroup.Function)
+                        {
+                            var commandTopic = $"{_topicRoot}/devices/{harmonyDevice.Label.Sluggify()}/{controlGroup.Name.Sluggify()}/{control.Name.Sluggify()}/set";
+                            device.Controls.Add(new ButtonControl { Name = controlGroup.Name + " " + control.Name, CommandTopic = commandTopic });
+
+                            // Add mapping for subscribed topic to Harmony control action, ignoring duplicates
+                            if (!_topicActionMap.ContainsKey(commandTopic))
+                                _topicActionMap.Add(commandTopic, control.Action);
+                        }
                     }
+
+                    hub.Devices.Add(device);
                 }
 
-                hub.Devices.Add(device);
+                // Add a Device for the hub itself at {_topicRoot}/activity/{activityName}
+                // Listen at topic {_topicRoot}/activity/+/set
+                var hubDevice = new Device { Name = "Harmony Hub " + _harmonyName };
+
+                // Map activities
+                var activitySelections = new Dictionary<string, string>();
+                foreach (var activity in _harmonyConfig.Activity)
+                {
+                    var commandTopic = $"{_topicRoot}/activity/{activity.Label.Sluggify()}/set";
+                    _topicActionMap.Add(commandTopic, activity.Id);
+                    hubDevice.Controls.Add(new ButtonControl { Name = "Activity: " + activity.Label, CommandTopic = commandTopic });
+
+                    activitySelections.Add(activity.Label, activity.Label);
+                }
+
+                // Add a Selector for the hub itself at {_topicRoot}/activity/set
+                // Listen at topic {_topicRoot}/activity/set
+                var currentActivityId = _client.GetCurrentActivityIdAsync().GetAwaiter().GetResult();
+                var currentActivity = _harmonyConfig.Activity.FirstOrDefault(x => x.Id == currentActivityId.ToString())?.Label;
+                var activityStateTopic = $"{_topicRoot}/activity";
+                var activityCommandTopic = $"{_topicRoot}/activity/set";
+                _topicActionMap.Add(activityCommandTopic, "Activity");
+
+                hubDevice.Controls.Add(new SensorControl { ValueTopic = activityStateTopic });
+                hubDevice.Controls.Add(new SelectorControl { CommandTopic = activityCommandTopic, ValueTopic = activityStateTopic, SelectionLabels = activitySelections });
+
+                hub.Devices.Add(hubDevice);
+
+                // Publish out device information for subscribers
+                _mqttClient.Publish(_topicRoot + "/homeAutio", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(hub, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All })), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+                _mqttClient.Publish(_topicRoot + "/activity", Encoding.UTF8.GetBytes(currentActivity), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
             }
-
-            // Add a Device for the hub itself at {_topicRoot}/activity/{activityName}
-            // Listen at topic {_topicRoot}/activity/+/set
-            var hubDevice = new Device { Name = "Harmony Hub " + _harmonyName };
-
-            // Map activities
-            var activitySelections = new Dictionary<string, string>();
-            foreach (var activity in _harmonyConfig.Activity)
+            catch (Exception ex)
             {
-                var commandTopic = $"{_topicRoot}/activity/{activity.Label.Sluggify()}/set";
-                _topicActionMap.Add(commandTopic, activity.Id);
-                hubDevice.Controls.Add(new ButtonControl { Name = "Activity: " + activity.Label, CommandTopic = commandTopic });
-
-                activitySelections.Add(activity.Label, activity.Label);
+                _log.Error(ex);
+                throw;
             }
-
-            // Add a Selector for the hub itself at {_topicRoot}/activity/set
-            // Listen at topic {_topicRoot}/activity/set
-            var currentActivityId = _client.GetCurrentActivityIdAsync().GetAwaiter().GetResult();
-            var currentActivity = _harmonyConfig.Activity.FirstOrDefault(x => x.Id == currentActivityId.ToString())?.Label;
-            var activityStateTopic = $"{_topicRoot}/activity";
-            var activityCommandTopic = $"{_topicRoot}/activity/set";
-            _topicActionMap.Add(activityCommandTopic, "Activity");
-
-            hubDevice.Controls.Add(new SensorControl { ValueTopic = activityStateTopic });
-            hubDevice.Controls.Add(new SelectorControl { CommandTopic = activityCommandTopic, ValueTopic = activityStateTopic, SelectionLabels = activitySelections });
-
-            hub.Devices.Add(hubDevice);
-
-            // Publish out device information for subscribers
-            _mqttClient.Publish(_topicRoot + "/homeAutio", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(hub, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All })), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
-            _mqttClient.Publish(_topicRoot + "/activity", Encoding.UTF8.GetBytes(currentActivity), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
         }
 
         #endregion
